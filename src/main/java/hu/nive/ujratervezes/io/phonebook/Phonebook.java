80141f1830a266ee6aaf1674b39ebe339ddc23e7
Andi <1vandidroid1@gmail.com>
2021-04-29 11:03:57 +0200

    Implement method

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,22 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+    void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null) throw new IllegalArgumentException();
+        if (output == null) throw new IllegalArgumentException();
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(output))) {
+            for (Map.Entry<String,String> entry : contacts.entrySet()) {
+                bufferedWriter.write(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+        }  catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+}
